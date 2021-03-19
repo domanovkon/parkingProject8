72c78a3f7e502b90cff0375e21e6311825357b87
domanovk <domanovkon@mail.ru>
2021-03-19 21:18:56 +0300

    vk

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::group(['middleware'=> 'guest'], function () {
     Route::get('/vk/auth', 'App\Http\Controllers\SocialController@index')->name('vk.auth');
-    Route::get('/vk/auth/callback', 'SocialController@callback');
+    Route::get('/vk/auth/callback', 'App\Http\Controllers\SocialController@callback');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
